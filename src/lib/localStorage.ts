002214897d6a4d6bb2dee1436e67a3ac908eb727
Mikel <mikeldiezgarcia@gmail.com>
2023-02-08 12:49:44 +0100

    remove sotrage a

--- src/lib/localStorage.ts
+++ src/lib/localStorage.ts
@@ -18,8 +18,8 @@ export const saveGameStateToLocalStorage = (
 
 export const loadGameStateFromLocalStorage = (isLatestGame: boolean) => {
   const key = isLatestGame ? gameStateKey : archiveGameStateKey
-  const state = localStorage.getItem(key)
-  return state ? (JSON.parse('{[],null}') as StoredGameState) : null
+  const state = null//localStorage.getItem(key)
+  return state ? (JSON.parse(state) as StoredGameState) : null
 }
 
 const gameStatKey = 'gameStats'
