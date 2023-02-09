88f07ac35f8281b7aad7cb97dc0eacd6892c54f9
Mikel <mikeldiezgarcia@gmail.com>
2023-02-09 09:30:20 +0100

    info panel added

--- src/components/modals/InfoModal.tsx
+++ src/components/modals/InfoModal.tsx
@@ -10,24 +10,23 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
   return (
     <BaseModal title="Como jugar" isOpen={isOpen} handleClose={handleClose}>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-       Adivina la palabra en 6 intentos, cada intento te brindara información sobre que letras contienen la palabra en funcion a su color. No se admiten ni conjugaciones ni plurales
-      <br>Mikel Diez (Arraiz)</br>
+       Adivina la palabra en 6 intentos, cada intento te brindara información sobre que letras contienen la palabra en funcion a de su color. No se admiten ni conjugaciones ni plurales
       </p>
 
       <div className="mb-1 mt-4 flex justify-center">
         <Cell
           isRevealing={true}
           isCompleted={true}
-          value="C"
+          value="W"
           status="correct"
         />
-        <Cell value="R" isCompleted={true} />
         <Cell value="E" isCompleted={true} />
-        <Cell value="M" isCompleted={true} />
         <Cell value="A" isCompleted={true} />
+        <Cell value="R" isCompleted={true} />
+        <Cell value="Y" isCompleted={true} />
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-       La letra C es correcta y esta en su lugar.
+       La letra W es correcta y esta en su lugar.
       </p>
 
       <div className="mb-1 mt-4 flex justify-center">
@@ -36,25 +35,25 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
         <Cell
           isRevealing={true}
           isCompleted={true}
-          value="S"
+          value="L"
           status="present"
         />
+        <Cell value="O" isCompleted={true} />
         <Cell value="T" isCompleted={true} />
-        <Cell value="A" isCompleted={true} />
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-       La letra S es correcta pero no esta en su lugar.
+       La letra L es correcta pero no esta en su lugar.
       </p>
 
       <div className="mb-1 mt-4 flex justify-center">
         <Cell value="V" isCompleted={true} />
-        <Cell value="I" isCompleted={true} />
-        <Cell value="S" isCompleted={true} />
-        <Cell isRevealing={true} isCompleted={true} value="T" status="absent" />
         <Cell value="A" isCompleted={true} />
+        <Cell value="G" isCompleted={true} />
+        <Cell isRevealing={true} isCompleted={true} value="U" status="absent" />
+        <Cell value="E" isCompleted={true} />
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-       La letra T no es correcta.
+       La letra U no es correcta.
       </p>
     </BaseModal>
   )
